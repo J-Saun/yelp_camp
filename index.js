f8f767dc2493727edb517f30831bc450ddd67dc4
Joshua <joshua_saun@protonmail.com>
2022-02-14 12:54:08 -0500

    added and tested views dir

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 const express = require('express');
 const res = require('express/lib/response');
 const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
   res.send('hello world');
