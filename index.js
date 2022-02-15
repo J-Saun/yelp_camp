80c63949553b3bdc9c50fe12f2f69ac807d50022
Joshua <joshua_saun@protonmail.com>
2022-02-15 18:09:36 -0500

    basic error handler and error template

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 const { nextTick } = require('process');
@@ -49,6 +50,8 @@ app.get('/campgrounds/new', (req, res) => {
 app.post(
   '/campgrounds',
   catchAsync(async (req, res, next) => {
+    if (!req.body.campground)
+      throw new ExpressError('Invalid campground data', 400);
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
@@ -89,8 +92,16 @@ app.delete('/campgrounds/:id', async (req, res) => {
   res.redirect('/campgrounds');
 });
 
+app.all('*', (req, res, next) => {
+  next(new ExpressError('Page Not Found', 404));
+});
+
 app.use((err, req, res, next) => {
-  res.send('Ruh Row');
+  const { statusCode = 500 } = err;
+  if (!err.message) {
+    err.message = 'Uh oh, something went wrong';
+  }
+  res.status(statusCode).render('error', { err });
 });
 
 app.listen(5500, () => {
